3680c74f903f331c74cd3889376a23e6915e330a
eduardo-sancho <eduardo.sancho@ucr.ac.cr>
2026-04-16 20:13:19 -0500

    se agregó el componente de guía médica a la página de inicio

--- plantastic-front/src/views/HomeView.vue
+++ plantastic-front/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <HeroSection />
   <OffersSection />
+  <MedicalGuide />
   <FeaturedProducts />
 </template>
 
 <script>
 import HeroSection from '../components/HeroSection.vue'
 import OffersSection from '../components/OffersSection.vue'
+import MedicalGuide from '../components/MedicalGuide.vue'
 import FeaturedProducts from '../components/FeaturedProducts.vue'
 
 export default {
@@ -14,6 +16,7 @@ export default {
   components: {
     HeroSection,
     OffersSection,
+    MedicalGuide,
     FeaturedProducts
   }
 }
