b4c1261cddbb808ec5cf1ca3b090beb3499e2bbd
eduardo-sancho <eduardo.sancho@ucr.ac.cr>
2026-06-04 15:52:58 -0600

    mount Pinia before routers in main.js

--- plantastic-front/src/main.js
+++ plantastic-front/src/main.js
@@ -4,4 +4,7 @@ import App from './App.vue'
 import router from './router'
 import './assets/styles/main.css'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
